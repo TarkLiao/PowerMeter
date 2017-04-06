e69f9bee332576d16fe8eabe1a73dbeb1d668809
Tark Liao <tark.liao@radinetcomm.com>
2017-04-06 11:42:52 +0800

    Test

--- app/src/main/java/com/radinet/ble_app/LedTextView.java
+++ app/src/main/java/com/radinet/ble_app/LedTextView.java
@@ -9,30 +9,4 @@ import android.util.AttributeSet;
 public class LedTextView extends android.support.v7.widget.AppCompatTextView
 {
 
-    public LedTextView(Context context)
-    {
-        super(context);
-        init(context);
-    }
-
-    public LedTextView(Context context, AttributeSet attrs)
-    {
-        super(context, attrs);
-        init(context);
-    }
-
-    public LedTextView(Context context, AttributeSet attrs, int defStyle)
-    {
-        super(context, attrs, defStyle);
-        init(context);
-    }
-
-    private void init(Context context)
-    {
-        //字体资源放入assets文件夹中
-        AssetManager am=context.getAssets();
-        Typeface font=Typeface.createFromAsset(am, "fonts/digital-7-italic.ttf");
-        setTypeface(font);
-    }
-
 }
